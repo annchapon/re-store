eb4977db1b9317138b87c4f42ea9e5ed9e54d02f
annchapon <annchapon@gmail.com>
2021-09-15 13:33:52 +0300

    refactor adding BookListContainer (component-container)

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -10,7 +10,23 @@ import ErrorIndicator from '../error-indicator';
 
 import './book-list.css';
 
-class BookList extends Component {
+const BookList = ({ books }) => {
+  return (
+    <ul className="book-list">
+      {
+        books.map((book) => {
+          return (
+            <li key={book.id}>
+              <BookListItem book={book} />
+            </li>
+          )
+        })
+      }
+    </ul>
+  );
+};
+
+class BookListContainer extends Component {
   componentDidMount() {
     /* 1. receive data
     const { bookstoreService } = this.props;
@@ -44,19 +60,7 @@ class BookList extends Component {
       return <ErrorIndicator />;
     }
 
-    return (
-      <ul className="book-list">
-        {
-          books.map((book) => {
-            return (
-              <li key={book.id}>
-                <BookListItem book={book} />
-              </li>
-            )
-          })
-        }
-      </ul>
-    );
+    return <BookList books={books} />;
   }
 }
 
@@ -97,4 +101,4 @@ const mapDispatchToProps = (dispatch, { bookstoreService }) => {
 export default compose(
   withBookstoreService(),
   connect(mapStateToProps, mapDispatchToProps)
-)(BookList);
+)(BookListContainer);
