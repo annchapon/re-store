1efc0ae92f1db06a522f7ba267fe53f04263a153
annchapon <annchapon@gmail.com>
2021-09-16 09:02:44 +0300

    add Monkey patching, Store Enhancer, Middleware

--- src/store.js
+++ src/store.js
@@ -2,6 +2,43 @@ import { createStore } from 'redux';
 
 import reducer from './reducers';
 
+/* Store Enhancer
+const enhancer = (createStore) => (...args) => {
+  const store = createStore(...args);
+  const originalDispatch = store.dispatch;
+  store.dispatch = (action) => {
+    if (typeof action === 'string') {
+      return originalDispatch({
+        type: action
+      });
+    }
+    return originalDispatch(action);
+  };
+  return store;
+};
+*/
+
+/* Middleware
+const logMiddleware = (store) = (dispathch) => (action) => {
+  console.log(action.type, store.getState());
+  return dispatch(action);
+};
+*/
+
 const store = createStore(reducer);
+
+/* Monkey patching
+const originalDispatch = store.dispatch;
+store.dispatch = (action) => {
+  if (typeof action === 'string') {
+    return originalDispatch({
+      type: action
+    });
+  }
+  return originalDispatch(action);
+};
+*/
+
+// store.dispatch('HELLO_WORLD');
 
 export default store;
